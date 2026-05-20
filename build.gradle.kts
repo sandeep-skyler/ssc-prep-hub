e2fbc45f8d9bd73497bffcaff038ff86f88b60bd
sandeep-skyler <sandeepsail3060@gmail.com>
2026-05-21 01:49:28 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,1 @@
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
-    }
-}
-
-// The repositories are already securely managed by settings.gradle.kts, 
-// so we don't need duplicate blocks here anymore!
+// Root build file where global plugins can be applied to sub-modules if needed.

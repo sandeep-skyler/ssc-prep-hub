3fbad0091d16137e681a554edf0a1a9334262c39
sandeep-skyler <sandeepsail3060@gmail.com>
2026-05-21 02:34:09 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,8 @@
 // Root build file where global plugins can be applied to sub-modules if needed.
+// Location: /build.gradle.kts (Project Root)
+plugins {
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+}

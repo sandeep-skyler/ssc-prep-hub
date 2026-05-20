74dfe9ba51560411c49be3f7f1f43b3f5c46a783
sandeep-skyler <sandeepsail3060@gmail.com>
2026-05-21 02:35:45 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+// Location: /settings.gradle.kts
 pluginManagement {
     repositories {
         google()
@@ -12,12 +13,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
-}
-
 rootProject.name = "SSC Prep Hub"
 include(":app")
